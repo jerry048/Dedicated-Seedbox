98718d8ee0fd1f81b6e64fb4ab2da6d08aa3cb6e
jerry048 <jw950@jerry048.com>
2023-02-02 11:39:27 +0800

    Update documentation

--- Install.sh
+++ Install.sh
@@ -5,54 +5,7 @@ tput sgr0; clear
 ## Load text color settings
 source <(wget -qO- https://raw.githubusercontent.com/jerry048/Seedbox-Components/main/Miscellaneous/tput.sh)
 
-## Check Root Privilege
-if [ $(id -u) -ne 0 ]; then 
-    warn_1; echo  "This script needs root permission to run"; normal_4 
-    exit 1 
-fi
-
-## Check Linux Distro
-distro_codename="$(source /etc/os-release && printf "%s" "${VERSION_CODENAME}")"
-if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] ; then
-	warn_1; echo "Only Debian 10/11 is supported"; normal_4
-	exit 1
-fi
-
-## Check Virtual Environment
-systemd-detect-virt > /dev/null
-if [ $? -eq 0 ]; then
-	warn_1; echo "Virtualization is detected, part of the script might not run"; normal_4
-fi
-
-## Grabing information
-username=$1
-password=$2
-cache=$3
-
-Cache1=$(expr $cache \* 65536)
-Cache2=$(expr $cache \* 1024)
-
-## Check existence of input argument in a Bash shell script
-
-if [ -z "$3" ]
-  then
-    warn_1; echo "Please fill in all 3 arguments accordingly: <Username> <Password> <Cache Size(unit:GiB)>"; normal_4
-    exit 1
-fi
-
-re='^[0-9]+$'
-if ! [[ $3 =~ $re ]] ; then
-   warn_1; echo "Cache Size has to be an integer"; normal_4
-   exit 1
-fi
-
-## Creating User
-warn_2
-pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-useradd -m -p "$pass" "$username"
-normal_2
-
-## Define Decision
+## Allow user to decide whether they would like to install a component or not
 function Decision {
 	while true; do
 		need_input; read -p "Do you wish to install $1? (Y/N):" yn; normal_1
@@ -63,6 +16,65 @@ function Decision {
 		esac
 	done
 }
+
+
+## Check Root Privilege since this script requires root privilege
+if [ $(id -u) -ne 0 ]; then 
+    warn_1; echo  "This script needs root permission to run"; normal_4 
+    exit 1 
+fi
+
+
+## Check Linux Distro since only Debian 10/11 is supported
+distro_codename="$(source /etc/os-release && printf "%s" "${VERSION_CODENAME}")"
+if [[ $distro_codename != buster ]] && [[ $distro_codename != bullseye ]] ; then
+	warn_1; echo "Only Debian 10/11 is supported"; normal_4
+	exit 1
+fi
+
+
+## Check Virtual Environment since part of the script might not work on virtual machine
+systemd-detect-virt > /dev/null
+if [ $? -eq 0 ]; then
+	warn_1; echo "Virtualization is detected, part of the script might not run"; normal_4
+fi
+
+
+## Grabing the informations to be used for BitTorrent client setup
+username=$1
+password=$2
+cache=$3
+
+## Grabing the informations of the server to be used in determining certain Tweaking parameters
+
+#Converting the cache size to Deluge's unit  (16KiB)
+Cache_de=$(expr $cache \* 65536)
+#Converting the cache to qBittorrent's unit (MiB)
+Cache_qB=$(expr $cache \* 1024)
+
+
+## Check existence of input argument in a Bash shell script
+
+#Check if user fill in all the required variables
+if [ -z "$3" ]
+  then
+    warn_1; echo "Please fill in all 3 arguments accordingly: <Username> <Password> <Cache Size(unit:GiB)>"; normal_4
+    exit 1
+fi
+
+#Preventing user from filling in float number as it would make converting the cache size to deluge be difficult
+re='^[0-9]+$'
+if ! [[ $3 =~ $re ]] ; then
+   warn_1; echo "Cache Size has to be an integer"; normal_4
+   exit 1
+fi
+
+
+## Creating User to contain the soon to be installed clients
+warn_2
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+useradd -m -p "$pass" "$username"
+normal_2
 
 
 ## Install Seedbox Environment
--- Tune.sh
+++ Tune.sh
@@ -27,7 +27,7 @@ while true; do
             "Deluge Tuning")
                 need_input; read -p "Enter username of your Deluge: " username
                 read -p "Cache Size (unit:GiB): " cache;
-                Cache1=$(expr $cache \* 65536)
+                Cache_de=$(expr $cache \* 65536)
                 Deluge_libtorrent; break
                 ;;
             "Tweaked BBR Install")
