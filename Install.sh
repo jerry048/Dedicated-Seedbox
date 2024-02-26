5a2909d59ca921ff20d2fc8eaab66f6d539b5cc2
jerry048 <61510650+jerry048@users.noreply.github.com>
2024-02-26 23:25:51 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -88,7 +88,7 @@ if [[ "$OS" =~ "Ubuntu" ]]; then #Ubuntu 20.04+ are supported
 fi
 
 ## Read input arguments
-while getopts "u:p:c:q:l:r:b:v:x:3:h" opt; do
+while getopts "u:p:c:q:l:rbvx3h" opt; do
   case ${opt} in
 	u ) # process option username
 		username=${OPTARG}
